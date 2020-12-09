f3a094a16a0cc2d8c799b3a3efd394f2b35f5c4d
Sanzhar <szhameledenov@gmail.com>
2020-12-09 23:03:38 +0600

    'lesson_4'

---
+++ app/src/main/java/com/example/lesson_3/MenuActivity4.java
@@ -0,0 +1,72 @@
+package com.example.lesson_3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MenuActivity4 extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_activity4);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent btnBack = new Intent(this, MainActivity.class);
+        startActivity(btnBack);
+    }
+
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.sub_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                Toast.makeText(this, "Settings menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_refresh:
+                Toast.makeText(this, "Refresh menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_delete:
+                Toast.makeText(this, "Delete menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.app_history:
+                Toast.makeText(this, "App History menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.app_service:
+                Toast.makeText(this, "App service menu clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+}
